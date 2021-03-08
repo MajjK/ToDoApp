34bfb359c2bc05076303639d505e1941253c7fce
MajjK <majuuk91@gmail.com>
2021-03-08 12:48:27 +0100

    Add Data Validation And Metadata Classes

---
+++ Models/PartialClasses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Models
+{
+    [MetadataType(typeof(TaskMetadata))]
+    public partial class Task
+    {
+    }
+
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+    }
+}
