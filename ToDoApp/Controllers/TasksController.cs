177d28875d3624d54b0db05954e357fe50ba507f
MajjK <majuuk91@gmail.com>
2021-03-17 22:42:55 +0100

    Rebuild Project Structure

---
+++ ToDoApp/Controllers/TasksController.cs
@@ -0,0 +1,249 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
+using AutoMapper;
+using ToDoApp.DB;
+using ToDoApp.DB.Model;
+using ToDoApp.ViewModel.Tasks;
+using System.Security.Claims;
+//Error method w auth controller
+//sha512, md5 kodowanie hasła
+
+namespace ToDoApp.Controllers
+{
+    public class TasksController : Controller
+    {
+        private ToDoDatabaseContext DbContext;
+        private readonly IMapper _mapper;
+
+        public TasksController(ToDoDatabaseContext context, IMapper mapper)
+        {
+            DbContext = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            int userId = int.Parse(this.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            ViewData["FinishSortParm"] = String.IsNullOrEmpty(sortOrder) ? "finish" : "";
+            ViewData["ObjectiveSortParm"] = sortOrder == "objective" ? "objective_desc" : "objective";
+            ViewData["DateSortParm"] = sortOrder == "date" ? "date_desc" : "date";
+            if (searchString != null)
+                page = 1;
+            else
+                searchString = currentFilter;
+            ViewData["CurrentFilter"] = searchString;
+
+            var tasks = this.GetSortedTasks(sortOrder, searchString).Where(task => task.UserId == userId);
+            var tasksViewModel = this.GetMappedViewModel(tasks);
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(await tasksViewModel.ToPagedListAsync(pageNumber, pageSize));
+        }
+
+        private List<TaskViewModel> GetMappedViewModel (IQueryable<DbTask> tasks)
+        {
+            List<TaskViewModel> tasksViewModel = new List<TaskViewModel>();
+            foreach (var item in tasks)
+            {
+                TaskViewModel taskViewModel = _mapper.Map<TaskViewModel>(item);
+                tasksViewModel.Add(taskViewModel);
+            }
+            return tasksViewModel;
+        }
+
+        private IQueryable<DbTask> GetSortedTasks(string sortOrder, string searchString)
+        {
+            var tasks = from s in DbContext.Tasks
+                        select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                if (DateTime.TryParse(searchString, out DateTime check_date))
+                    tasks = tasks.Where(s => s.Objective.Contains(searchString) || s.ClosingDate.Value.Date.Equals(check_date));
+                else
+                    tasks = tasks.Where(s => s.Objective.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "finish":
+                    tasks = tasks.OrderBy(s => s.Finished).ThenBy(s => s.ClosingDate);
+                    break;
+                case "objective":
+                    tasks = tasks.OrderBy(s => s.Objective);
+                    break;
+                case "objective_desc":
+                    tasks = tasks.OrderByDescending(s => s.Objective);
+                    break;
+                case "date":
+                    tasks = tasks.OrderBy(s => s.ClosingDate).ThenByDescending(s => s.Finished);
+                    break;
+                case "date_desc":
+                    tasks = tasks.OrderByDescending(s => s.ClosingDate).ThenByDescending(s => s.Finished);
+                    break;
+                default:
+                    tasks = tasks.OrderByDescending(s => s.Finished).ThenBy(s => s.ClosingDate);
+                    break;
+            }
+            return tasks;
+        }
+
+        public async Task<IActionResult> Create(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await DbContext.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            TaskViewModel taskViewModel = new TaskViewModel { UserId = (int)id };
+            return View(taskViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId, Objective, Description, ClosingDate")] TaskViewModel taskViewModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    DbTask taskModel = _mapper.Map<DbTask>(taskViewModel);
+                    DbContext.Add(taskModel);
+                    await DbContext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View(taskViewModel);
+        }      
+        
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await DbContext.Tasks
+                .Include(task => task.User)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(task => task.TaskId == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            TaskViewModel taskViewModel = _mapper.Map<TaskViewModel>(task);
+            return View(taskViewModel);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await DbContext.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            TaskViewModel taskViewModel = _mapper.Map<TaskViewModel>(task);
+            return View(taskViewModel);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taskToUpdate = await DbContext.Tasks.FirstOrDefaultAsync(s => s.TaskId == id);
+            if (await TryUpdateModelAsync<DbTask>(taskToUpdate, "",
+                s => s.Objective, s => s.Description, s => s.AdditionDate, s => s.ClosingDate, s => s.Finished))
+            {
+                try
+                {
+                    await DbContext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                        "Try again, and if the problem persists, " +
+                        "see your system administrator.");
+                }
+            }
+
+            TaskViewModel taskViewModel = _mapper.Map<TaskViewModel>(taskToUpdate);
+            return View(taskViewModel);
+        }
+
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await DbContext.Tasks.AsNoTracking().FirstOrDefaultAsync(s => s.TaskId == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Try again, and if the problem persists " +
+                    "see your system administrator.";
+            }
+
+            TaskViewModel taskViewModel = _mapper.Map<TaskViewModel>(task);
+            return View(taskViewModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var task = await DbContext.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            try
+            {
+                DbContext.Tasks.Remove(task);
+                await DbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException)
+            {
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+            }
+        }
+
+    }
+}
---
+++ ToDoApp/Startup.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.DB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+//Scaffold-DbContext "User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=ToDoDatabase;" Npgsql.EntityFrameworkCore.PostgreSQL -OutputDir Models
+
+
+namespace ToDoApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<ToDoDatabaseContext>(options => options.UseNpgsql(this.Configuration["ToDoDatabase"]));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                { 
+                    options.LoginPath = "/Auth/Login";
+                });
+            services.AddSession();
+            services.AddMvcCore(options =>
+            { 
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Auth/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Auth}/{action=Login}/{id?}");
+            });
+        }
+    }
+}
