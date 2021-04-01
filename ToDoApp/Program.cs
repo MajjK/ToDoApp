fb9187a3abc7c071acb68b26518e92e8ead336b3
MajjK <majuuk91@gmail.com>
2021-04-02 00:51:56 +0200

    Move Database Initialization To Initial Migration

--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -16,9 +16,6 @@ namespace ToDoApp
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-
-            CreateDbIfNotExists(host);
-
             host.Run();
         }
 
@@ -28,23 +25,5 @@ namespace ToDoApp
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-
-        private static void CreateDbIfNotExists(IHost host)
-        {
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<ToDoDatabaseContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred creating the DB.");
-                }
-            }
-        }
     }
 }
